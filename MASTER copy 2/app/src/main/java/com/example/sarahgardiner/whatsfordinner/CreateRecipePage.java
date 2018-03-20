895f0436c0d7acaf7779cdca32669fb7e3464d43
Sarah Gardiner <sgardine@umw.edu>
2018-03-20 12:43:24 -0400

    added logs in create recipe page

--- MASTER copy 2/app/src/main/java/com/example/sarahgardiner/whatsfordinner/CreateRecipePage.java
+++ MASTER copy 2/app/src/main/java/com/example/sarahgardiner/whatsfordinner/CreateRecipePage.java
@@ -3,6 +3,7 @@ package com.example.sarahgardiner.whatsfordinner;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -25,28 +26,33 @@ public class CreateRecipePage extends AppCompatActivity {
         //-------------- get recipe name ---------------------------------
         EditText mEdit   = (EditText)findViewById(R.id.recipeName);
         String rName = mEdit.getText().toString();
+        Log.d("myTag", "RecipeName: "+ rName);
 
         //-------------- get serving size ---------------------------------
         EditText mEdit2   = (EditText)findViewById(R.id.recipeServings);
         String ser = mEdit2.getText().toString();
         int rServing = Integer.parseInt(ser);
+        Log.d("myTag", "Recipe serving: "+ rServing);
 
         //-------------- get recipe instructions ----------------------------
         EditText mEdit3   = (EditText)findViewById(R.id.recipeInstuctions);
         String rInstructions = mEdit3.getText().toString();
+        Log.d("myTag", "rInstructions: "+ rInstructions);
 
         //---------------- get category ------------------------------------
         Spinner cat = (Spinner)findViewById(R.id.Category);
         String rCategory = cat.getSelectedItem().toString();
+        Log.d("myTag", "rCategory: "+ rCategory);
 
         //--------------- recipe object created ----------------------------
         if(rName.trim().length() == 0 || ser.trim().length() == 0 || rInstructions.trim().length() == 0 || rCategory.trim().length() == 0){
-            finish();
-        }
-        else {
             Recipe r = new Recipe(rName, rInstructions, rCategory, rServing, AddIngredientPage.IngredientList);
             //add recipe to arraylist thing
-            //Recipes.add(r);
+            Recipes.add(r);
+
+        }
+        else {
+            finish();
         }
 
         AddIngredientPage.IngredientList.clear();
